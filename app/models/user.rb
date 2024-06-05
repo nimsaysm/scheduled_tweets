0a60d5dcc0fb421b3173b98280dd27f54e869732
nimsaysm <yasmin.soraya14@gmail.com>
2024-06-05 08:49:58 -0300

    Fix: Making the email field mandatory

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# DB fields: 
+# email:string
+# password_digest:string (password after bcrypt)
+
+# virtual atributes:
+# password:string
+# password_confirmation:string
+
+class User < ApplicationRecord
+    has_secure_password
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ , message: "must be a valid email address" }
+end
