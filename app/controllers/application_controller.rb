3ec1f80996da159e94f759df8b1dd1f9f1d6b05d
nimsaysm <yasmin.soraya14@gmail.com>
2024-06-06 15:06:02 -0300

    Add: Edit and reset passwords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
     
+    def require_user_logged_in!
+        # if Current.user be Null or nil, will be redirected to sign in
+        redirect_to sign_in_path, alert: "You must be signed in to do that" if Current.user.nil?
+    end
+    
     def set_current_user
         if session[:user_id]
             Current.user = User.find_by(id: session[:user_id])
